de6777b7d2dc8feb0dba3aaa43bb6c41c79303ac
Hiep Nguyen <hiepnguyen@Hieps-MacBook-Pro.local>
2021-07-31 12:39:03 -0700

    moved to Promises

--- app.js
+++ app.js
@@ -9,39 +9,64 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static('public'));
 
 //CALL BACKS
-function getUsers(cb){
-  fs.readFile('data.json', 'utf8', (err, data) => {
-    if (err) return cb(err);
-    const users = JSON.parse(data);
-    return cb(null, users);
-  });
+// function getUsers(cb){
+//   fs.readFile('data.json', 'utf8', (err, data) => {
+//     if (err) return cb(err);
+//     const users = JSON.parse(data);
+//     return cb(null, users);
+//   });
+// }
+
+// app.get('/', (req,res) => {
+//   getUsers((err, users) => {
+//     if (err) {
+//       res.render('error', {error: err});
+//     } else {
+//       res.render('index', {title: "Users", users: users.users})
+//     }
+//   })
+// }); 
+
+// app.get('/:id', (req, res) => {
+//   getUser(req.params.id, (err, user)=>{
+//     if(err){
+//       res.render('error', {error: err});
+//     } else {
+//       getFollowers(user, (err, followers) =>{
+//         if(err){
+//           res.render('error', {error: err});
+//         } else {
+//           res.render('profile', {title: "Profile Page", user: user, followers: followers});
+//         }
+//       }); 
+//     }
+//   });
+//  });
+
+
+function getUsers() {
+  return new Promise((resolve, reject) => {
+    fs.readFile('data.json', 'utf-8', (err, data) =>{
+      if (err) {
+        reject(err);
+      } else {
+        const users = JSON.parse(data);
+        resolve(users);
+      }
+    })
+  })
 }
 
-app.get('/', (req,res) => {
-  getUsers((err, users) => {
-    if (err) {
-      res.render('error', {error: err});
-    } else {
+app.get('/', (req, res) => {
+  getUsers()
+    .then((users) => {
+      // throw new Error("Noooo!");
       res.render('index', {title: "Users", users: users.users})
-    }
-  })
-}); 
-
-app.get('/:id', (req, res) => {
-  getUser(req.params.id, (err, user)=>{
-    if(err){
+    })
+    .catch((err) => {
       res.render('error', {error: err});
-    } else {
-      getFollowers(user, (err, followers) =>{
-        if(err){
-          res.render('error', {error: err});
-        } else {
-          res.render('profile', {title: "Profile Page", user: user, followers: followers});
-        }
-      }); 
-    }
-  });
- });
+    });
+});
 
 
 app.listen(3000, () => console.log('App listening on port 3000!'));
